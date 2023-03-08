aede2a2d11ca3a0413d211150d7644ef5f5c69ae
Laura Brea <laurabrea17@gmail.com>
2023-03-07 21:37:08 -0500

    committing to add to gitHub

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+import ColorBlock from './ColorBlock'
+//import './App.css';
+function App() {
+  let colors = [
+    'violet', 'blue', 
+    'lightblue', 'green', 
+    'greenyellow', 'yellow', 
+    'orange', 'red'
+  ]
+
+  let colorMap = colors.map((color, i) => {
+      return (
+        <ColorBlock color={color} />
+      )
+    })
+  return (
+    <div className="App">
+        {colorMap}
+    </div>
+  );
+}
+
+export default App;
